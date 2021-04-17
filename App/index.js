435d0aaad6653b459a738b51bc65e6929dca5089
MiguelCaputo <miguelcaputo@LaptopMiguel.localdomain>
2021-04-17 17:41:53 -0400

    Fixed index

--- App/index.js
+++ App/index.js
@@ -5,7 +5,7 @@ const myParser = require("body-parser");
 //Initializing variables
 var app = express();
 
-const port = 3001;
+const port = 3001 || process.env.PORT;
 const MongoClient = require("mongodb").MongoClient;
 var cors = require("cors");
 const { ObjectID } = require("bson");
@@ -87,6 +87,6 @@ app.get("/login", async (req, res) => {
 });
 
 //Checking that the server is working
-app.listen(process.env.PORT || port, () => {
+app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
 });
